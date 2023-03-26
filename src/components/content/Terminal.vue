0e93386d6e94b569e993b73601fa96aa646fcbe2
Trent Brew <tbrew1023@gmail.com>
2023-03-26 16:27:25 -0500

    fix: update location

--- src/components/content/Terminal.vue
+++ src/components/content/Terminal.vue
@@ -15,7 +15,7 @@ export default {
       :text="[
         `Hi!`,
         `My name's Trent`,
-        `I'm a Software Engineer based in LA`,
+        `I'm a Software Engineer based in San Diego, CA`,
         `Welcome to my website ^-^`,
         `Feel free to click around & explore my work...`,
       ]"
