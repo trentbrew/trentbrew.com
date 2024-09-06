7977a6673ced648c03edb8b2e8e09262ee198cf1
Trent Brew <tbrew1023@gmail.com>
2024-09-06 00:56:15 -0700

    englessness

--- src/components/desktop/Desktop.vue
+++ src/components/desktop/Desktop.vue
@@ -51,7 +51,7 @@
             icon: 'music.svg',
             label: "What I'm Listening To",
             embed:
-              'https://open.spotify.com/embed/playlist/7mmPNDSTN9j1kVSnEVElZu?si=eOBntkOnToCOpjB4oVqymA?utm_source=generator&theme=0',
+              'https://open.spotify.com/embed/album/51CQQ3tQLRZlZJZ5jcpoGE?si=eOBntkOnToCOpjB4oVqymA?utm_source=generator&theme=0',
             windowWidth: 680,
             windowHeight: 420,
           },
