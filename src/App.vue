3c844da39497626e7ef6e72207ace2495623a79f
Trent Brew <tbrew1023@gmail.com>
2024-10-12 11:34:56 -0700

    cuttothechase

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,6 @@
     data() {
       return {}
     },
-    mounted() {
-      this.checkin()
-    },
-    methods: {
-      checkin: async () => {
-        const info = {
-          timestamp: new Date().toISOString(),
-          timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-          localTime: new Date().toLocaleString(),
-          userAgent: navigator.userAgent,
-          isMobile: /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
-          referrer: document.referrer,
-          crumbs: await window.cookieStore.getAll(),
-        }
-        try {
-          const body = JSON.stringify(info)
-          await fetch('https://api.trentbrew.com/guestbook', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body,
-          })
-        } catch (err) {
-          console.warn('guestbook error:', err)
-        } finally {
-          console.log('<3')
-        }
-      },
-    },
   }
 </script>
 
